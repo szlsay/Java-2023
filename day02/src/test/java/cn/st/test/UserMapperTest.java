6a2daafad2bf8aa4644d0222e1da656e91b6a5c7
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-14 17:32:24 +0800

    mybatis

---
+++ day02/src/test/java/cn/st/test/UserMapperTest.java
@@ -0,0 +1,66 @@
+package cn.st.test;
+
+import cn.st.mapper.UserMapper;
+import cn.st.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+
+public class UserMapperTest {
+    @Test
+    public void testSelect() throws IOException {
+        //1. 获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2. 获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //3. 获取Mapper接口的代理对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        //4. 执行方法
+        String username = "zhangsan";
+        String password = "123";
+
+        User user = userMapper.select(username, password);
+//        User user = userMapper.select(new HashSet());
+//        User user = userMapper.selectNew(username);
+        System.out.println(user);
+
+        //5. 释放资源
+        sqlSession.close();
+    }
+
+
+
+    @Test
+    public void testSelectById() throws IOException {
+        //1. 获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2. 获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //3. 获取Mapper接口的代理对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        //4. 执行方法
+
+        User user = userMapper.selectById(1);
+
+        System.out.println(user);
+
+        //5. 释放资源
+        sqlSession.close();
+    }
+}
