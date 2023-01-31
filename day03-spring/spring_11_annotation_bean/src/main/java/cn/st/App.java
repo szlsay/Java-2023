51797fc0ce843208e8f0fe40cf9da9073805ca8a
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-31 12:19:42 +0800

    spring_12_annotation_bean_manager

---
+++ day03-spring/spring_11_annotation_bean/src/main/java/cn/st/App.java
@@ -0,0 +1,16 @@
+package cn.st;
+
+import cn.st.config.SpringConfig;
+import cn.st.dao.BookDao;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        BookDao bookDao1 = ctx.getBean(BookDao.class);
+        BookDao bookDao2 = ctx.getBean(BookDao.class);
+        System.out.println(bookDao1);
+        System.out.println(bookDao2);
+        ctx.close();
+    }
+}
