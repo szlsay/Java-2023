06d0cd147bd772a8cdd71d525d587b15eee9626a
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-31 11:29:58 +0800

    spring_10_container

---
+++ day03-spring/spring_10_container/src/main/java/cn/st/dao/impl/BookDaoImpl.java
@@ -0,0 +1,13 @@
+package cn.st.dao.impl;
+
+import cn.st.dao.BookDao;
+
+public class BookDaoImpl implements BookDao {
+    public BookDaoImpl() {
+        System.out.println("constructor");
+    }
+
+    public void save() {
+        System.out.println("book dao save ..." );
+    }
+}
