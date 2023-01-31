a622bcc983065d49928d8745d4138d10a74d463d
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-31 13:04:01 +0800

    spring_15_spring_mybatis

---
+++ day03-spring/spring_15_spring_mybatis/src/main/java/cn/st/App2.java
@@ -0,0 +1,18 @@
+package cn.st;
+
+import cn.st.config.SpringConfig;
+import cn.st.domain.Account;
+import cn.st.service.AccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App2 {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        AccountService accountService = ctx.getBean(AccountService.class);
+
+        Account ac = accountService.findById(1);
+        System.out.println(ac);
+    }
+}
---
+++ day03-spring/spring_15_spring_mybatis/src/main/java/cn/st/config/SpringConfig.java
@@ -0,0 +1,14 @@
+package cn.st.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("cn.st")
+//@PropertySource：加载类路径jdbc.properties文件
+@PropertySource("classpath:jdbc.properties")
+@Import({JdbcConfig.class,MybatisConfig.class})
+public class SpringConfig {
+}
