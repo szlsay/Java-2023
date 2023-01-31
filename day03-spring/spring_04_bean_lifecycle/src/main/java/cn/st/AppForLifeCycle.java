5d114c9c890e97f2b3149c328e0b133177099736
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-31 08:50:07 +0800

    spring_04_bean_lifecycle

---
+++ day03-spring/spring_04_bean_lifecycle/src/main/java/cn/st/AppForLifeCycle.java
@@ -0,0 +1,17 @@
+package cn.st;
+
+import cn.st.dao.BookDao;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppForLifeCycle {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+        bookDao.save();
+        //注册关闭钩子函数，在虚拟机退出之前回调此函数，关闭容器
+        //ctx.registerShutdownHook();
+        //关闭容器
+        ctx.close();
+    }
+}
---
+++ day03-spring/spring_04_bean_lifecycle/src/main/java/cn/st/service/BookService.java
@@ -0,0 +1,5 @@
+package cn.st.service;
+
+public interface BookService {
+    public void save();
+}
