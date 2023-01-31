bb255d0b1a6eb4b4b400a8cc01524221f7fefdeb
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-31 14:15:28 +0800

    spring_22_aop_advice_data

---
+++ day03-spring/spring_22_aop_advice_data/src/main/java/cn/st/dao/impl/BookDaoImpl.java
@@ -0,0 +1,14 @@
+package cn.st.dao.impl;
+
+import cn.st.dao.BookDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDaoImpl implements BookDao {
+
+    public String findName(int id,String password) {
+        System.out.println("id:"+id);
+        if(true)throw new NullPointerException();
+        return "st";
+    }
+}
