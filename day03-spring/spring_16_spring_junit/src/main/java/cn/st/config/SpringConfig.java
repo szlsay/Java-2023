337da9a94d12c198e4b5ef2a1551a3f7cc51fbd4
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-31 13:10:26 +0800

    spring_16_spring_junit

---
+++ day03-spring/spring_16_spring_junit/src/main/java/cn/st/config/SpringConfig.java
@@ -0,0 +1,13 @@
+package cn.st.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("cn.st")
+@PropertySource("classpath:jdbc.properties")
+@Import({JdbcConfig.class,MybatisConfig.class})
+public class SpringConfig {
+}
