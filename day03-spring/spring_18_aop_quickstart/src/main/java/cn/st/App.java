032f98ee9a3a85e8ea8082a188140d2194fea31a
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-31 13:23:58 +0800

    spring_18_aop_quickstart

---
+++ day03-spring/spring_18_aop_quickstart/src/main/java/cn/st/App.java
@@ -0,0 +1,17 @@
+package cn.st;
+
+import cn.st.config.SpringConfig;
+import cn.st.dao.BookDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        BookDao bookDao = ctx.getBean(BookDao.class);
+        bookDao.update();
+//        bookDao.save();
+        System.out.println(bookDao);
+        System.out.println(bookDao.getClass());
+    }
+}
