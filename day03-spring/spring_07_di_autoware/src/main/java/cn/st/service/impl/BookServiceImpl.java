768a242453d28e736ffd7c242e157dcb58807f3e
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-01-31 10:50:07 +0800

    spring_07_di_autoware spring_08_di_collection

---
+++ day03-spring/spring_07_di_autoware/src/main/java/cn/st/service/impl/BookServiceImpl.java
@@ -0,0 +1,17 @@
+package cn.st.service.impl;
+
+import cn.st.dao.BookDao;
+import cn.st.service.BookService;
+
+public class BookServiceImpl implements BookService{
+    private BookDao bookDao;
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    public void save() {
+        System.out.println("book service save ...");
+        bookDao.save();
+    }
+}
