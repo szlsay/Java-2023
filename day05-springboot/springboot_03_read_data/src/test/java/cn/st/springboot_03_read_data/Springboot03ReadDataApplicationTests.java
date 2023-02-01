cd4f0fc0ef692f4318d0e0b3e732f25dcec1c96a
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-01 17:08:41 +0800

    springboot_03_read_data

---
+++ day05-springboot/springboot_03_read_data/src/test/java/cn/st/springboot_03_read_data/Springboot03ReadDataApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.st.springboot_03_read_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03ReadDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
