16d4348ee82ab3652363fd271c55ff195d40eb94
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-03 15:59:24 +0800

    springdataredis_demo

---
+++ day08-redis/springdataredis_demo/src/main/java/cn/st/SpringdataredisDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataredisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataredisDemoApplication.class, args);
+    }
+
+}
